9654eaf1a332b7b00644b7eb24bffd8883b40fdf
JannekRC <jan.nek@live.dk>
2021-09-05 17:24:26 +0300

    Second commit

--- src/main/java/com/example/bookstore/domain/Book.java
+++ src/main/java/com/example/bookstore/domain/Book.java
@@ -11,11 +11,11 @@ public class Book {
 	}
 	
 	public Book(String title, String author, int year, String isbn, double price) {
-		title = this.title;
-		author = this.author;
-		year = this.year;
-		isbn = this.isbn;
-		price = this.price;
+		this.title = title;
+		this.author = author;
+		this.year = year;
+		this.isbn = isbn;
+		this.price = price;
 	}
 	
 
