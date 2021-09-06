4eb797974769bbde28729d25044b25a0f711d94d
JannekRC <jan.nek@live.dk>
2021-09-06 12:00:20 +0300

    Third commit

--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -1,14 +1,14 @@
 package com.example.bookstore.web;
 
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.bookstore.domain.Book;
 
 public class BookController {
 
-	@GetMapping("/index")
-	public String formAccess(Model model) {
+	@RequestMapping(value="/index")
+	public String showIndex(Model model) {
 		model.addAttribute("book", new Book());
 		return "form";
 	}
